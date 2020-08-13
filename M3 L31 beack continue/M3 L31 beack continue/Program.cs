fabb89e566564e7129267fcaa79c83e2f417f3db
Dany CADIOU <66691002+dacadiou27@users.noreply.github.com>
2020-08-13 12:09:54 -0600

    Add files via upload

---
+++ M3 L31 beack continue/M3 L31 beack continue/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace M3_L31_beack_continue
+{
+    class Program
+    {
+        static int Prime(int n)
+        {
+            int i = 2;
+            if (n == 1) return 0;
+            while (i <=Math.Sqrt (n))
+            {
+                if (n % i == 0) return 0;
+                i++;
+            }
+            return 1;
+        }
+
+        static int RandomNumber ()
+        {
+            Random obj = new Random();
+            int j = obj.Next(1, 101);
+            return j;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number of of prime numbers that you want to find : ");
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            //while (count <= n)
+            do
+            {
+                int number = RandomNumber();
+
+                if (Prime(number) == 0) 
+                //if (number%2==0 ) // for odd numbers
+                {
+                    continue;
+                }
+                count++;
+                Console.WriteLine("This is the {0} number - {1} was prime.",count, number );
+                
+            } while (count < n) ;
+
+                Console.Read();
+
+        }
+    }
+}
