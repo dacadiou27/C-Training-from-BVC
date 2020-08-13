7834392eba806f72bc145c29832720875039d5c4
Dany CADIOU <66691002+dacadiou27@users.noreply.github.com>
2020-08-13 11:40:46 -0600

    Add files via upload

---
+++ M2 L21 Example 3 Invalid Age/M2 L21 Example 3 Invalid Age/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace M2_L21_Example_3_Invalid_Age
+{
+    class Program
+    {
+		static void Main(string[] args) // Example of mixed/complex-if-else Statement
+		{
+			string name;
+			int age;
+			bool validAge = false;
+			Console.WriteLine("Enter your username: ");
+			name = Console.ReadLine();
+			if (name == "Mahbub")
+			{
+				Console.WriteLine("Hi Mahbub!");
+				Console.WriteLine("Enter your age: ");
+				validAge = int.TryParse(Console.ReadLine(), out age);// retur true if valid int or false if invalid int // out is use to bypass value
+				if (validAge)
+				{
+					Console.WriteLine("{0}, your registered age is: {1}. Thank you for your input.", name, age);
+				}
+				else
+				{
+					Console.WriteLine("Invalid Input. Please enter valid age in your next attempt.");
+				}
+			}
+			else if (name.Length == 0)
+			{
+				Console.WriteLine("No username was entered.");
+			}
+			else
+			{
+				Console.WriteLine("The user {0}, has not been registered.", name);
+			}
+			Console.Read();
+		}
+	}
+}
---
+++ M2 L21 If/M2 L21 If/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace M2_L21_If
+{
+    class Program
+    {
+        static void Main(string[] args) 
+
+        {
+            Console.WriteLine("Emter your name :");
+            string name = Console.ReadLine();
+
+
+            if (name.Length == 0)
+            {
+                Console.WriteLine("No username was entered.");
+            }
+
+            Console.WriteLine("Emter your age :");
+            int age = int.Parse(Console.ReadLine());
+
+
+
+            if (name == "Mahbub" || name == "Susan" || name == "Dany")
+            {
+                Console.WriteLine("Hi {0} !", name);
+            }
+            else
+            {
+                Console.WriteLine("The user {0} is not registered!", name);
+                if (age <= 18)
+                {
+                    Console.WriteLine("You must be 18 years of age for the registration!");
+                }
+                else
+                {
+                    Console.WriteLine("Hi {0}, do you want to register?", name);
+                }
+            }
+        } 
+        
+
+        /*static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter mark: :");
+            double mark = double.Parse(Console.ReadLine());
+
+            if (mark >= 90)
+            {
+                Console.WriteLine("A+");
+            }
+            else if (mark >= 85 )
+            {
+                Console.WriteLine("A");
+            }
+            else if (mark >= 80 )
+            {
+                Console.WriteLine("A-");
+            }
+            else
+            {
+                Console.WriteLine("F");
+            }
+
+
+            /*if (mark >= 90)
+            {
+                Console.WriteLine("A+");
+            }
+             if (mark >= 85  && mark < 90)
+            {
+                Console.WriteLine("A");
+            }
+            if (mark >= 80 && mark < 85)
+            {
+                Console.WriteLine("A-");
+            }
+            if (mark < 80)
+            {
+                Console.WriteLine("F");
+            }*/
+        //Console.Read();
+
+        // }
+
+
+        /*
+        static void Main(string[] args)
+        {
+            string a = "ABCD";
+            string b = "AAAAAAAAAAAAAAAAAAAAAAAA";
+            //bool c = true;
+            //bool d = true;
+
+            if (a.CompareTo(b) == 0)
+            {
+                Console.WriteLine("Strings are equal!");
+            }
+            else if (a.CompareTo(b) == 1)
+            {
+                Console.WriteLine("The first string is bigger than the second");
+
+            }
+            //else if (a.CompareTo(b) == -1)
+            else
+            {
+                Console.WriteLine("The second string is bigger than the first");
+
+            }
+            Console.Read();
+        }
+        */
+
+        }
+}
